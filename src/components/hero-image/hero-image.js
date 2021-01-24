063849b47d0fb7d50e2dd1f27b99c2856adacdd9
Sean <seanom96@gmail.com>
2021-01-24 11:34:12 +0000

    hero image resizes to screen

--- src/components/hero-image/hero-image.js
+++ src/components/hero-image/hero-image.js
@@ -1,9 +1,10 @@
 import React from "react";
-
+import styles from "./hero-image.module.css";
 const HeroImage = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <img
+        className={styles.image}
         alt="the ordinary offer"
         src="https://cdn11.bigcommerce.com/s-63354/product_images/uploaded_images/beauty-features-mobile-banner-the-ordinary.jpg?t=1610015632&_ga=2.213000412.976479337.1609747081-1182666886.1602669683"
       />
